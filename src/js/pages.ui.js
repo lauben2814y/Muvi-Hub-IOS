0298fe96d8dd4fe1ec5bc8bb510b3aa633d39600
lauben2814y <lauben2814y@gmail.com>
2024-10-09 12:00:27 +0300

    update 5 files

--- src/js/pages.ui.js
+++ src/js/pages.ui.js
@@ -121,82 +121,33 @@ class PageUI {
         })
     }
     renderCollection(mediaArr, mediaDOM, mediaPage) {
-        var dateProperty;
-        var titleProperty;
-        var mediaType;
-        var shortenName;
         mediaArr.forEach(media => {
-            if (media.title) {
-                titleProperty = "title";
-                dateProperty = "release_date";
-                mediaType = "movie";
-                shortenName = media.title;
-            } else {
-                shortenName = media.name;
-                mediaType = "tv";
-                titleProperty = "name";
-                dateProperty = "first_air_date";
-            }
-            var date = media[dateProperty];
-            var poster = media.poster_path
-            var id = media.id;
+            var titleProperty = media.release_date ? 'title' : 'name';
+            var dateProperty = media.release_date ? 'release_date' : 'first_air_date';
+            var type = media.release_date ? 'movie' : 'tv';
+
             var list = document.createElement('li');
-
-            var itemLink = document.createElement('a');
-            itemLink.className = 'item-link movie_poster';
-            itemLink.dataset.id = id;
-            itemLink.dataset.page = mediaPage;
-            itemLink.dataset.name = mediaType;
-            // Create item content div
-            var itemContent = document.createElement('div');
-            itemContent.classList.add('item-content');
-            itemContent.style.pointerEvents = 'none';
-            // Create item media div
-            var itemMedia = document.createElement('div');
-            itemMedia.classList.add('item-media');
-            // Create image element
-            var img = document.createElement('img');
-            img.className = "list_poster_img lazy"
-            img.style.borderRadius = '8px';
-            img.src = './pages/placeholder.webp';
-            img.setAttribute('data-src', `https://image.tmdb.org/t/p/w500${poster}`);
-            // Append image to item media
-            itemMedia.appendChild(img);
-            // Create item inner div
-            var itemInner = document.createElement('div');
-            itemInner.classList.add('item-inner');
-            // Create item title row div
-            var itemTitleRow = document.createElement('div');
-            itemTitleRow.classList.add('item-title-row');
-            // Create item title div
-            var itemTitle = document.createElement('div');
-            itemTitle.classList.add('item-title');
-            itemTitle.textContent = shortenName;
-            // Append item title to item title row
-            itemTitleRow.appendChild(itemTitle);
-            // Append item title row to item inner
-            itemInner.appendChild(itemTitleRow);
-            // Create item subtitle div
-            var itemSubtitle = document.createElement('div');
-            itemSubtitle.classList.add('item-subtitle');
-            itemSubtitle.textContent = mediaType;
-            // Append item subtitle to item inner
-            itemInner.appendChild(itemSubtitle);
-            // Create item text div
-            var itemText = document.createElement('div');
-            itemText.classList.add('item-text');
-            itemText.textContent = media.overview;
-            // Append item text to item inner
-            itemInner.appendChild(itemText);
-            // Append item media, item inner to item content
-            itemContent.appendChild(itemMedia);
-            itemContent.appendChild(itemInner);
-            // Append item content to anchor element
-            itemLink.appendChild(itemContent);
-
-            list.appendChild(itemLink);
-            mediaDOM.appendChild(list)
-        })
+            list.innerHTML = `
+                <a class="item-link movie_poster" data-id="${media.id}" data-page="${mediaPage}" data-name="${type}">
+                    <div class="item-content" style="pointer-events: none;">
+                        <div class="item-media">
+                        <img class="list_poster_img lazy"
+                            src="./pages/placeholder.webp"
+                            data-src="https://image.tmdb.org/t/p/w500${media.poster_path}"
+                            style="border-radius: 8px;">
+                        </div>
+                        <div class="item-inner">
+                            <div class="item-title-row">
+                            <div class="item-title">${media[titleProperty]}</div>
+                            </div>
+                            <div class="item-subtitle">${type}</div>
+                            <div class="item-text">${media.overview}</div>
+                        </div>
+                    </div>
+                </a>
+            `
+            mediaDOM.appendChild(list);
+        });
     }
     renderEpisodes(mediaDOM, mediaArr, id, showOverview) {
         mediaArr.forEach(episode => {
@@ -225,63 +176,32 @@ class PageUI {
             mediaDOM.appendChild(list);
         })
     }
-    renderWishMedia(mediaArr, mediaDOM) {
-        var dateProperty;
-        var titleProperty;
-        var mediaType;
-        var shortenName;
-        function shortenText2(string, maxLength) {
-            if (string.length > maxLength) {
-                return string.substring(0, maxLength) + '...';
-            }
-            return string;
-        }
+    renderWishMedia(mediaArr, mediaDOM, mediaPage) {
         mediaArr.forEach(media => {
-            if (media.title) {
-                titleProperty = "title";
-                dateProperty = "release_date";
-                mediaType = "movie";
-                shortenName = media.title;
-            } else {
-                shortenName = media.name;
-                mediaType = "tv";
-                titleProperty = "name";
-                dateProperty = "first_air_date";
-            }
-            var title = shortenText2(shortenName, 14);
-            var date = media[dateProperty];
-            var poster = media.poster_path
-            var id = media.id;
-            var mediaCard = document.createElement('div');
-            mediaCard.className = "media_card";
+            var titleProperty = media.release_date ? 'title' : 'name';
+            var type = media.release_date ? 'movie' : 'tv';
 
-            var deleteBtn = document.createElement('i');
-            deleteBtn.className = "icon material-icons delete_button";
-            deleteBtn.innerText = "delete";
-
-            var media = document.createElement('div');
-            media.className = "media";
-            var img = document.createElement('img');
-            img.className = "movie_poster lazy";
-            img.dataset.id = id;
-            img.dataset.name = mediaType;
-            img.dataset.page = "main";
-            img.setAttribute('data-src', `https://image.tmdb.org/t/p/w500${poster}`);
-            img.src = './placeholder.jpg';
-            media.appendChild(img);
-
-            var details = document.createElement('div');
-            details.className = "details";
-            details.innerHTML = `
-            <h4 class="title">${title}</h4>
-            <p>${date}</p>
+            var list = document.createElement('li');
+            list.innerHTML = `
+                <a class="item-link movie_poster" data-id="${media.id}" data-page="${mediaPage}" data-name="${type}">
+                    <div class="item-content" style="pointer-events: none;">
+                        <div class="item-media">
+                        <img class="list_poster_img lazy"
+                            src="./pages/placeholder.webp"
+                            data-src="https://image.tmdb.org/t/p/w500${media.poster_path}"
+                            style="border-radius: 8px;">
+                        </div>
+                        <div class="item-inner">
+                            <div class="item-title-row">
+                            <div class="item-title">${media[titleProperty]}</div>
+                            </div>
+                            <div class="item-subtitle">${type}</div>
+                            <div class="item-text">${media.overview}</div>
+                        </div>
+                    </div>
+                </a>
             `
-            mediaCard.appendChild(deleteBtn);
-            mediaCard.appendChild(media);
-            mediaCard.appendChild(details);
-
-            // content.appendChild(mediaCard);
-            mediaDOM.appendChild(mediaCard);
+            mediaDOM.appendChild(list);
         });
     }
 }
